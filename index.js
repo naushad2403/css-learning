a22843507a458a0c31f55d46ace0b524de9a0c3c
Naushad Ali <naushadali2403@gmail.com>
2023-04-26 09:37:39 +0530

    - vw, vh concept learned

---
+++ index.js
@@ -0,0 +1,19 @@
+html, body {
+    margin: 0;
+    padding: 0;
+    min-width: 100%;
+    min-height: 100%;
+}
+
+.top_bar {
+    background-color: green;
+    min-width: 100px;
+    min-height: 100px;
+}
+
+.top_bar :first-child {
+     background-color: red;
+    min-width: 50%;
+    min-height: 100%;
+    opacity: 1;
+}
